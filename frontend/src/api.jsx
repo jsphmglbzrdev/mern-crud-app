4d40cf652e53d23ad5f9011c6f854784ac01592e
jsphmglbzrdev <jsphmglbzr.dev@gmail.com>
2026-02-20 15:10:04 +0800

    debug

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks"
+  baseURL: "https://mern-crud-app-sj2y.onrender.com"
 });
 
 export default API;
